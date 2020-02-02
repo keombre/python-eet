1797951a47de33d54ab46e39b76d25771f955acf
keombre <keombre8@gmail.com>
2020-02-02 02:24:57 +0100

    construct semi-valid xml

--- eet/binding.py
+++ eet/binding.py
@@ -1,8 +1,21 @@
-from dataclasses import dataclass
 from . import types
 from typing import Optional
 
-import xml.etree.cElementTree as ET
+from dataclasses import dataclass
+from pathlib import Path
+from lxml import etree
+
+import re
+import base64
+import hashlib
+
+# Let's stay cryptic
+from cryptography import x509
+from cryptography.x509 import Certificate
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
 
 @dataclass
 class Trzba:
@@ -46,45 +59,89 @@ class Trzba:
             "encoding": "base16"
         }
     }
-    Bkp: types.BkpType = None
-    Pkp: types.PkpType = None
 
-def buildDataXml(sale: Trzba):
+class Soap:
 
-    root = ET.Element("eet:Trzba")
+    def __init__(self, root_ca: Certificate, private_key: RSAPrivateKey):
+        if not isinstance(root_ca, Certificate):
+            raise ValueError("root_ca is not instance of Certificate")
+        self.root_ca = root_ca
 
-    ET.SubElement(root, "eet:Hlavicka", {k: str(v) for k, v in Trzba.Hlavicka.items() if v is not None})
-    ET.SubElement(root, "eet:Data", {k: str(v) for k, v in Trzba.Data.items() if v is not None})
-
-    codes = ET.SubElement(root, "eet:KontrolniKody")
-    ET.SubElement(codes, "eet:pkp", sale.KontrolniKody["pkp"]).text = sale.Pkp
-    ET.SubElement(codes, "eet:bkp", sale.KontrolniKody["bkp"]).text = sale.Bkp
-
-    return root
-
-def signEnvelope():
-    pass
-
-def buildEnvelope(data: ET.Element):
-    root = ET.Element("soap:Envelope", {
-        "xmlns:soap": "http://www.w3.org/2003/05/soap-envelope/",
-        "soap:encodingStyle": "http://www.w3.org/2003/05/soap-encoding"
-    })
-    header = ET.SubElement(root, "soap:Header")
-    body = ET.SubElement(root, "soap:Body")
-    body.append(data)
+        if not isinstance(private_key, RSAPrivateKey):
+            raise ValueError("private_key is not instance of RSAPrivateKey")
+        self.private_key = private_key
     
-    return root
+    def __get_ca_cert(self):
+        return self.root_ca.public_bytes(serialization.Encoding.PEM).replace(b"-----BEGIN CERTIFICATE-----", b"").replace(b"-----END CERTIFICATE-----", b"").replace(b"\n", b"")
 
-def demo():
-    trzba = Trzba()
-    trzba.Hlavicka["uuid_zpravy"] = types.UUIDType("e23e5a5a-08d7-4a08-844d-2b6c6b60621d")
-    trzba.Hlavicka["dat_odesl"] = types.dateTime.utcnow()
-    trzba.Hlavicka["prvni_zaslani"] = types.boolean(True)
-    trzba.Hlavicka["overeni"] = types.boolean(False)
+    def build(self, sale: Trzba):
+        return etree.tostring(self._build_envelope(self._build_data_element(sale)))
 
-    trzba.Pkp = "asdf"
+    def _build_data_element(self, sale: Trzba):
+        root = etree.Element("{http://fs.mfcr.cz/eet/schema/v3}Trzba", nsmap={
+            "eet": "http://fs.mfcr.cz/eet/schema/v3",
+            "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "xsd": "http://www.w3.org/2001/XMLSchema"
+        })
 
-    print(ET.tostring(buildEnvelope(buildDataXml(trzba))))
+        etree.SubElement(root, "{http://fs.mfcr.cz/eet/schema/v3}Hlavicka", {k: str(v) for k, v in Trzba.Hlavicka.items() if v is not None})
+        etree.SubElement(root, "{http://fs.mfcr.cz/eet/schema/v3}Data", {k: str(v) for k, v in Trzba.Data.items() if v is not None})
+        codes = etree.SubElement(root, "{http://fs.mfcr.cz/eet/schema/v3}KontrolniKody")
 
-demo()
+        sign = self._calc_sign(sale)
+        etree.SubElement(codes, "{http://fs.mfcr.cz/eet/schema/v3}pkp", sale.KontrolniKody["pkp"]).text = base64.b64encode(sign)
+        etree.SubElement(codes, "{http://fs.mfcr.cz/eet/schema/v3}bkp", sale.KontrolniKody["bkp"]).text = self._calc_bkp(sign)
+
+        return root
+
+    def _build_envelope(self, data: etree.Element):
+
+        binary_token = "X509-asdf"
+        cert_token = "id-asdf"
+
+        tree = etree.parse(str(Path(__file__).parent.absolute().joinpath('soap_template.xml')))
+        root = tree.getroot()
+        
+        # add body
+        body = root.find(".//{http://schemas.xmlsoap.org/soap/envelope/}Body")
+        body.set("{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id", cert_token)
+        body.append(data)
+
+        # add cert
+        BinarySecurityToken = root.find(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}BinarySecurityToken")
+        BinarySecurityToken.set("{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id", binary_token)
+        BinarySecurityToken.text = self.__get_ca_cert()
+
+        # cert ref
+        root.find(".//{http://www.w3.org/2000/09/xmldsig#}Reference").set("URI", cert_token)
+
+        # digest
+        body_text = etree.tostring(body, method='c14n', exclusive=True, with_comments=False)
+        digest = base64.b64encode(hashlib.sha256(body_text).digest())
+        root.find(".//{http://www.w3.org/2000/09/xmldsig#}DigestValue").text = digest
+
+        # header signed
+        signed_info = root.find(".//{http://www.w3.org/2000/09/xmldsig#}SignedInfo")
+        signed_text = etree.tostring(signed_info, method='c14n', exclusive=True, with_comments=False)
+        signed_signed = self.private_key.sign(signed_text, padding.PKCS1v15(), hashes.SHA256())
+        root.find(".//{http://www.w3.org/2000/09/xmldsig#}SignatureValue").text = base64.b64encode(signed_signed)
+
+        root.find(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}Reference").set("URI", "#" + binary_token)
+        
+        return root
+    
+    def _calc_sign(self, sale: Trzba):
+        text = "{0}|{1}|{2}|{3}|{4}|{5}".format(
+            sale.Data["dic_popl"],
+            sale.Data["id_provoz"],
+            sale.Data["id_pokl"],
+            sale.Data["porad_cis"],
+            sale.Data["dat_trzby"],
+            sale.Data["celk_trzba"]
+        )
+        return self.private_key.sign(text.encode('utf8'), padding.PKCS1v15(), hashes.SHA256())
+    
+    def _calc_bkp(self, sign):
+        digest = hashlib.sha1(sign).hexdigest()
+        return ("{0}-{1}-{2}-{3}-{4}".format(digest[0:8], digest[8:16], digest[16:24], digest[24:32], digest[32:])).upper()
+
